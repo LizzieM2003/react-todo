42516090bf4c5b1b42054434ed360a799df09b24
LizzieM2003 <liz_mendes2001@yahoo.co.uk>
2016-08-26 15:39:44 +0000

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -17,6 +17,36 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos :  [];
-    
+  },
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    // filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      // if (searchText.length === 0) {
+      //   return todo;
+      // } else if (todo.text.toLowerCase().indexOf(searchText) !== -1) {
+      //   return todo;
+      // }
+      var todoText = todo.text.toLowerCase();
+      return searchText.length === 0 || todoText.indexOf(searchText) > -1;
+    });
+
+    // Sort todos with non completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    return filteredTodos;
   }
 };
